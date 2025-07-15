070795658761d54235b4943225faad2f18217c06
ZacksShen <zacks.shen@gmail.com>
2025-07-15 12:47:29 -0400

    Update timer function to count both sync or async functions

--- China/utilities/tools.py
+++ China/utilities/tools.py
@@ -1,17 +1,38 @@
+import asyncio
 import functools
 import time
+from typing import Callable, Any
 
 
-def timer(func):
-    """Print the runtime of the decorated function"""
+def timer(func: Callable) -> Callable:
+    """
+    Timer decorator that works with both sync and async functions.
+    """
+    if asyncio.iscoroutinefunction(func):
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        runtime = end - start
-        print(f"⏱️  {func.__name__} runtime: {runtime:.4f} s")
-        return result
+        @functools.wraps(func)
+        async def async_wrapper(*args, **kwargs) -> Any:
+            start_time = time.perf_counter()
+            try:
+                result = await func(*args, **kwargs)
+                return result
+            finally:
+                end_time = time.perf_counter()
+                exec_time = end_time - start_time
+                print(f"⏱️  Function '{func.__name__}' runtime: {exec_time:.4f} s")
 
-    return wrapper
+        return async_wrapper
+    else:
+
+        @functools.wraps(func)
+        def sync_wrapper(*args, **kwargs) -> Any:
+            start_time = time.perf_counter()
+            try:
+                result = func(*args, **kwargs)
+                return result
+            finally:
+                end_time = time.perf_counter()
+                exec_time = end_time - start_time
+                print(f"⏱️  Function '{func.__name__}' runtime: {exec_time:.4f} s")
+
+        return sync_wrapper
